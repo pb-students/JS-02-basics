40150704cbec6909ddf4ab71cd03484c1ac0f4a4
SiwyKot41 <65611113+SiwyKot41@users.noreply.github.com>
2021-10-25 16:25:05 +0200

    Update subs1.js

--- subs1.js
+++ subs1.js
@@ -1,18 +1,18 @@
-// for (let i = 0; i <= 100; i++) {
-//   if ((i % 2 === 0) && (i % 3 === 0)) {
-//     console.log("FizBuz")
-//     continue;
-//   }
-//
-//   if ((i % 2 === 0)) {
-//     console.log("Fiz");
-//     continue;
-//   }
-//
-//   if (i % 3 === 0) {
-//     console.log("Buz");
-//     continue;
-//   }
-//
-//   console.log(i);
-// }
+ for (let i = 0; i <= 100; i++) {
+   if ((i % 2 === 0) && (i % 3 === 0)) {
+     console.log("FizBuz")
+     continue;
+   }
+
+   if ((i % 2 === 0)) {
+     console.log("Fiz");
+     continue;
+   }
+
+   if (i % 3 === 0) {
+     console.log("Buz");
+     continue;
+   }
+
+   console.log(i);
+ }
